7e2e345189020d71ac47a12e5570cb1e74966e37
Novaedra <novaedra@gmail.com>
2018-11-07 14:52:50 +0100

    Lfirstcommit

---
+++ inc/footer.php
@@ -0,0 +1,48 @@
+<footer class="footer">
+	<div class="footer-left">
+		<div class ="footer-logo"><a href="index.php"><img  src="img/logotreswag.png" alt=""></a></div>
+		<p class="footer-links">
+			<a href="index.php">Home</a>
+			·
+			<a href="http://forum.doctissimo.fr/sante/vaccination/liste_sujet-1.htm">Blog</a>
+			·
+			<a href="#">Qui sommes nous ?</a>
+			·
+			<a href="#">Contact</a>
+		</p>
+		<p class="footer-company-name">VAX &copy; 2015</p>
+	</div>
+
+	<div class="footer-center">
+		<h3>Où nous trouver ?</h3>
+		<div>
+			<i class="material-icons">&#xe0c8;</i>
+			<p><span>7 Place St-Marc</span> Paris, France</p>
+		</div>
+		<div>
+			<i class="material-icons">&#xe0b0;</i>
+			<p>+123 456 789</p>
+		</div>
+		<div class="mail">
+			<i class="material-icons">&#xe0be;</i>
+			<p><a href="mailto:vax@gmail.com">vax@gmail.com</a></p>
+		</div>
+		<div class="maps">
+			<div class="mapouter"><div class="gmap_canvas"><iframe width="500" height="150" id="gmap_canvas" src="https://maps.google.com/maps?q=Rouen%207%20place%20saint%20marc&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
+		</div>
+	</div>
+
+	<div class="footer-right">
+		<h3>A propos</h3>
+		<p class="about"> Vax est une plateforme d'information et de communication sur les vaccins présents en France. Plus de 10000 professionels de la santé sont actuellement inscrits sur VAX. </p>
+
+		<div class="footer-icons">
+			<div class="icon facebook"><a href="#"></a></div>
+			<div class="icon twitter"><a href="#"></a></div>
+			<div class="icon youtube"><a href="#"></a></div>
+			<div class="icon rss"><a href="#"></a></div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
