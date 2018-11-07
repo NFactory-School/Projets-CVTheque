477a3158138c07281275bf87ee6ffacfeb2cfe43
novaedra <44367513+novaedra@users.noreply.github.com>
2018-11-07 15:31:00 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,60 +1,83 @@
-<?php include 'inc/header.php'; ?>
+<?php
+include ('inc/pdo.php');
+include ('inc/fonction.php');
+include ('inc/header.php');
+/*ceci est un test a supprimer*/
+?>
 
     <div class="intro">
 
-        <h2 class="titre-intro">Bienvenue sur <span>VAX</span></h2>
-        <p class="texte-intro">Gérer vos vaccins en ligne.</p>
-
+        <h2>Bienvenue sur <span>VAX</span></h2>
+		<p>Gérer vos vaccins<br></p>
+		<span class="texte-intro">en ligne.</span>
     </div>
 
-<div class="formulaire">
+<div class="login-wrap">
+	<div class="login-html">
+	<form action="" method="post">
 
-	<div class="signup">
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked>
+		<label for="tab-1" class="tab">S'incrire</label>
+		<input id="tab-2" type="radio" name="tab" class="sign-up">
+		<label for="tab-2" class="tab">Connexion</label>
 
-		<form action="#">
+		<div class="login-form">
 
-				<div class="">
-					<input type="text" placeholder="Adresse email">
-					<br/>
-                </div>
+			<div class="sign-in-htm">
 
-				<div class="">
-					<input type="password" placeholder="Mot de passe">
-					<br/>
+				<div class="group">
+					<span class="error"><?php if(!empty($errors['pseudo'])){echo $errors['pseudo'];};?></span>
+					<input type="text" placeholder="Pseudo" name="pseudo" value="<?php if(!empty($_POST['pseudo'])) {echo $_POST['pseudo'];}?>"><br>
+					<?php br(); ?>
+				</div>	
+
+				<div class="group">
+					<span class="error"><?php if(!empty($errors['mail'])){echo $errors['mail'];};?></span>
+					<input type="text" placeholder="Adresse E-mail" name="mail" value="<?php if(!empty($_POST['mail'])) {echo $_POST['mail'];}?>"><br>
+					<?php br(); ?>
 				</div>
 
-				<div class="">
-					<input type="password" placeholder="Confirmer mot de passe">
-					<br/>
+				<div class="group">
+				<span class="error"><?php if(!empty($errors['mdp'])){echo $errors['mdp'];};?></span>
+					<input type="password" placeholder="Mot de passe" name="mdp" value=""><br>
+					<?php br(); ?>
 				</div>
 
-			    <div class="">
-					<input type="submit" value="Inscription">
-					<br/>
+				<div class="group">
+					<input type="password" placeholder="Répéter mot de passe" name="mdpV" value=""><br> 
 				</div>
-		</form>
+
+				<div class="group">
+					<input type="submit" name="submit" value="S'inscrire">
+				</div>
+				<div class="hr"></div>
+			</div>
+			
+	</form>
+
+	<form action="" method="post">
+
+			<div class="sign-up-htm">
+
+				<div class="group">
+					<input type="text" placeholder="Pseudo" name="pseudo" value="<?php if(!empty($_POST['pseudo'])) {echo $_POST['pseudo'];}?>"><br>
+					<span class="error"><?php if(!empty($errors['pseudo'])){echo $errors['pseudo'];};?></span><?php br(); ?>
+				</div>	
+
+				<div class="group">
+					<input type="text" placeholder="Mot de passe" name="mdp" value=""><br>
+					<span class="error"><?php if(!empty($errors['mdp'])){echo $errors['mdp'];};?></span><?php br(); ?>
+				</div>
+
+				<div class="group">
+					<input type="submit" class="send" value="Se connecter" name="connexion">
+				</div>
+				<div class="hr"></div>
+			</div>
+		</div>
+	</form>
 	</div>
-
-	<div class="login">
-
-		<form action="#">
-
-				<div class="">
-					<input type="text" placeholder="Adresse email">
-					<br/>
-                </div>
-
-				<div class="">
-					<input type="password" placeholder="Mot de passe">
-					<br/>
-                </div>
-
-				<div class="">
-					<input type="submit" value="Connexion">
-				</div>
-		</form>
-	</div>
-
 </div>
 
-<?php include 'inc/footer.php'; ?>
+<?php
+include 'inc/footer.php'; ?>
