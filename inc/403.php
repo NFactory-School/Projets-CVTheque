a74f5ccbd723464cdff4eaef5cc9fa7debc4aa69
Novaedra <novaedra@gmail.com>
2018-11-09 16:48:45 +0100

    404

---
+++ inc/403.php
@@ -0,0 +1,24 @@
+<?php 
+include ('inc/pdo.php');
+include ('inc/fonction.php');
+include ('inc/header.php'); ?>
+
+<a class="back" href ="index.php">
+    <div class="container">
+      <div class="spooky">
+        <div class="body"><h2 class="noAccess">403</h2>
+          <div class="eyes"></div>
+          <div class="mouth"></div>
+          <div class="feet">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div class="shadow"></div>
+    </div>
+  </a>
+    <h2 class ="noAccess">Access not granted ! </br><span>click me to go home</span></h2>
+
+<?php include ('inc/footer.php') ?>
