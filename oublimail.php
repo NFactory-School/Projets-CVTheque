dacc987b9ee0efe6cbc4cf4f02c393305dce342f
Novaedra <novaedra@gmail.com>
2018-11-09 16:30:20 +0100

    index1

---
+++ oublimail.php
@@ -0,0 +1,47 @@
+<?php
+include('inc/pdo.php');
+include('inc/fonction.php');
+
+include ('inc/header.php');
+
+$errors = array();
+if(!empty($_POST['submit'])){
+    $mail = trim(strip_tags($_POST['mail']));
+  if(!empty($_POST['mail'])){
+    if(!filter_var($mail, FILTER_VALIDATE_EMAIL)){
+        $errors['mail'] = "Veuillez entrer un mail valide";
+      }else{
+        // requete sql
+        $sql = "SELECT mail, token FROM nf_user WHERE mail = :mail";
+        $query = $pdo -> prepare($sql);
+        $query -> bindValue(':mail', $mail, PDO::PARAM_STR);
+        $query -> execute();
+        $user = $query -> fetch();
+        if(!empty($user)){
+          $body = '<p>Veuillez cliquer sur ce '.'<a class="myButton" href = "oublimdp.php?mail='.urlencode($user['mail']).'&token='.urlencode($user['token']).'">lien</a>'.'</p>';
+        }else {
+          $errors['mail'] = 'Cette adresse mail n\'est pas présente dans notre base de données';
+        }
+      }
+    }else{
+      $errors['mail'] = "Veuillez renseigner une adresse mail";
+    }
+}
+?>
+
+<!-- Formulaire avec recup du mail et envoi du token et du mail. -->
+<form class="" action="" method="post">
+<fieldset>
+<legend>Mot de passe oublié </legend>
+<?php if(!empty($_POST['mail']) && !empty($user)){
+  echo $body = '<p>Veuillez cliquer sur ce '.'<a class="myButton" href = "oublimdp.php?mail='.urlencode($user['mail']).'&token='.urlencode($user['token']).'">lien</a>'.'</p>';
+}else{?>
+<label for="mail">Mail </label>
+<input type="text" name="mail" value="">
+<span class="error"><?php if(!empty($errors['mail'])){echo $errors['mail'];}?></span>
+<input class="myButton" type="submit" name="submit" value="Envoyer">
+<span><?php if(!empty($body)){echo $body;};?></span>
+<?php } ?></fieldset>
+</form>
+
+<?php include ('inc/footer.php');
---
+++ redirection.php
@@ -0,0 +1,11 @@
+<?php
+include('inc/pdo.php');
+include('inc/fonction.php');
+
+include ('inc/header.php');
+
+echo" Bravo ! Vous avez rempli le formulaire !"; br();?>
+
+<a href="index.php">retour à l'accueil</a>
+
+<?php include ('inc/footer.php');
