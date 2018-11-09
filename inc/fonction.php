75357619fbc15b4c40c2ccb677f2ab0c911e728b
Novaedra <novaedra@gmail.com>
2018-11-09 16:46:32 +0100

    fonction

--- inc/fonction.php
+++ inc/fonction.php
@@ -1,97 +1,93 @@
 <?php
-
 function br(){
-
   echo '<br/>';
 }
 
 function division(int $a,int $b){
-
 $result = $a/$b;
-return($result);
-}
+return($result);}
 
 function tab($array){
-
   echo '<pre>';
   print_r ($array);
-  echo '</pre>';
-}
+  echo '</pre>';}
 
 function inc($i){
-
     $i++;
-    return($i);
-}
+    return($i);}
 
 $errors = array();
-
-function validationTxt($errors,$data,$min,$max,$key,$empty = true){
+function vTxt($errors,$data,$min,$max,$key,$empty = true){
 if (!empty($data)){
-
   if(strlen($data) < $min){
     $errors[$key]= 'min  '.$min.' caracteres ';
-  }
-  
-  elseif (strlen($data) > $max ) {
+  }elseif (strlen($data) > $max ) {
     $errors=[$key]='max '.$max.' caractere';
   }
-
+}else {
+  if($empty){
+  $errors[$key]='Veuillez renseigner ce champ';}
+}
+  return $errors;
 }
 
-else if($empty){
-  $errors[$key]='Veuillez renseigner ce champ';
-}
-
-return $errors;
-}
-  
-
-function vmail($error,$value,$min,$max,$key) {
-
+function vMail($error,$value,$min,$max,$key) {
   if (!empty($value)) {
-
-    if (filter_var($value, FILTER_VALIDATE_EMAIL)){
-
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)){
       $value = trim(strip_tags($value));
-
       if (is_string($value) && strlen($value) <= $min) {
         $error[$key] = 'error : moins de '.$min.' caractères';
       }
-
       elseif (is_string($value) && strlen($value) >= $max ){
         $error[$key] = 'error : plus de '.$max.' caractères';
       }
-
       else {
-
         $error[$key] = 'error : vide';
       }
-
     return $error;
-
     }
   }
 }
 
-function table($key){
-  return 
-  '<tr><td>'.$key['title'].'</td>
-  <td>'.$key['auteur'].'</td>
-  <td>'.$key['content'].'</td>
-  <td>'.$key['created_at'].'</td>
-  <td>'.$key['updated_at'].'</td>
-  <td>'.$key['statu'].'</td>
-  <td><a href="editpost.php?id='.$key['id'].'">Modifier</a></td></tr>';
-  
+function vMdp($error,$data,$min,$max,$key){
+  if (!empty($data)) {
+    if (is_string($data) && strlen($data) <= $min) {
+      $error[$key] = 'error : moins de '.$min.' caractères';
+    }
+    elseif (is_string($data) && strlen($data) >= $max ){
+      $error[$key] = 'error : plus de '.$max.' caractères';
+    }
+  }
+  else {
+    $error[$key] = 'error : vide';
+  }
+  return $error;
 }
 
-function generateRandomString(){
-  $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  $string = "";
-  for($i=0; $i<20; $i++){
-      $randNum = mt_rand(0, strlen($characters) -1);
-      $string .= $characters[$randNum];
-  }
-  return $string;
+function generateRandomString($length){
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $charsLength = strlen($characters) -1;
+    $string = "";
+    for($i=0; $i<$length; $i++){
+        $randNum = mt_rand(0, $charsLength);
+        $string .= $characters[$randNum];
+    }
+    return $string;
 }
+
+function isLogged(){
+  if(!empty($_SESSION['user']['id']) && !empty($_SESSION['user']['pseudo']) && !empty($_SESSION['user']['mail']) && !empty($_SESSION['user']['status']) && !empty($_SESSION['user']['ip'])) {
+    if($_SESSION['user']['ip'] = $_SERVER['REMOTE_ADDR']){
+    return true;
+  }
+}
+return false;
+}
+
+function isAdmin() {
+  if (!isLogged() && $_SESSION['user']['status']!='admin'){
+    header('Location:../403.php');
+  }
+}
+
+?>
