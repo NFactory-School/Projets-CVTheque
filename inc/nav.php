b370fc6eddc057f512b6638e850964e3095508d7
Novaedra <novaedra@gmail.com>
2018-11-09 16:40:35 +0100

    index1

---
+++ inc/nav.php
@@ -0,0 +1,9 @@
+<nav>
+  <div class="row">
+    <a href="#">Home</a>
+    <a href="#">Votre carnet</a>
+    <a href="#">Blog</a>
+    <a href="#">A propos</a>
+    <a href="#">Contact</a>
+  </div>
+</nav>
