d663b086066c3ab1a9734a9cbb7fce443284c638
Novaedra <novaedra@gmail.com>
2018-11-09 16:45:04 +0100

    nav

--- inc/nav.php
+++ inc/nav.php
@@ -1,9 +1,12 @@
 <nav>
-  <div class="row">
-    <a href="#">Home</a>
-    <a href="#">Votre carnet</a>
-    <a href="#">Blog</a>
-    <a href="#">A propos</a>
-    <a href="#">Contact</a>
-  </div>
+  <?php
+
+        if (!islogged()) {
+          echo '<li><p> <a href="incription.php"> Inscription </a> </li>
+          <p> <a href="connection.php"> Connexion </a> </p>';
+        }else{
+          echo '<p>Bienvenue : '. $_SESSION['user']['pseudo'] .' </p> <br/>
+          <p> <a href="deconnection.php"> Deconnexion </a> </p>';
+        }
+  ?>
 </nav>
